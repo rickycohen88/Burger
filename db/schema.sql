3556b065efb8d0e59703fdab4342acacc499fae0
RickyCohen <rickycohen88@gmail.com>
2021-01-08 19:32:29 -0600

    started project

---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+
+CREATE TABLE burgers(
+    id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR (30),
+    devoured BOOLEAN DEFAULT 0,
+    PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name) VALUES ('Hamburger'),('Cheeseburger'),('BaconCheeseburger');
